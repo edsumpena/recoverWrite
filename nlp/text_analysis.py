f012502a5c9efb26e62e255b34b939eccfaf6b8e
Daniel Wang <danielwang1590@gmail.com>
2023-09-16 11:54:35 -0400

    add constructor

--- nlp/text_analysis.py
+++ nlp/text_analysis.py
@@ -1,7 +1,5 @@
-import streamlit as st
 from transformers import pipeline
 from transformers import AutoModelForSequenceClassification
-from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
@@ -10,6 +8,15 @@ class TextAnalysis(object):
     """
     Class that analyzes the journal entries of patients. 
     """
+
+    def __init__(self):
+        MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+        self.sentiment_tokenizer = AutoTokenizer.from_pretrained(MODEL)
+        self.sentiment_config = AutoConfig.from_pretrained(MODEL)
+        # PT
+        self.sentiment_model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+        self.summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
     # Preprocess text (username and link placeholders)
     def preprocess(text):
@@ -20,7 +27,7 @@ class TextAnalysis(object):
             new_text.append(t)
         return " ".join(new_text)
     
-    def sentiment_analysis(journal_entry:str):
+    def sentiment_analysis(self, journal_entry:str):
         """Takes in a body of text and identifies the probability of such an entry being negative, netural, or positive sentiment.
         
         Parameters
@@ -33,18 +40,12 @@ class TextAnalysis(object):
         sentiment_scores : dict
             The softmax probabilities for the journal entry to be classified with a particular sentiment {negative, neutral, positive}.
         """
-
-        MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
-        tokenizer = AutoTokenizer.from_pretrained(MODEL)
-        config = AutoConfig.from_pretrained(MODEL)
-        # PT
-        model = AutoModelForSequenceClassification.from_pretrained(MODEL)
         #model.save_pretrained(MODEL)
 
         # pipe = pipeline('sentiment-analysis')
         text = TextAnalysis.preprocess(journal_entry) # THIS IS THE TEXT THAT PROMPTS THE USER
-        encoded_input = tokenizer(text, return_tensors='pt')
-        output = model(**encoded_input)
+        encoded_input = self.sentiment_tokenizer(text, return_tensors='pt')
+        output = self.sentiment_model(**encoded_input)
         scores = output[0][0].detach().numpy()
         scores = softmax(scores)
 
@@ -53,14 +54,14 @@ class TextAnalysis(object):
         # ranking = ranking[::-1]
         sentiment_scores = {} # THIS IS THE DICTIONARY WITH THE SENTIMENT SCORES
         for i in range(scores.shape[0]):
-            l = config.id2label[i]
+            l = self.sentiment_config.id2label[i]
             s = scores[i]
             sentiment_scores[f'{l}'] = np.round(float(s), 4)
 
         return sentiment_scores
         
 
-    def summarize(journal_entry:str):
+    def summarize(self, journal_entry:str):
         """
         Summarizes a body of text to include some of the main points and sentiments.
 
@@ -74,8 +75,7 @@ class TextAnalysis(object):
         summarized_text : str
             Summary of the patient's journal entry.
         """
-        summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-        summarized_text = summarizer(journal_entry, max_length=200, min_length=30, do_sample=False)
+        summarized_text = self.summarizer(journal_entry, max_length=200, min_length=30, do_sample=False)
 
         return summarized_text[0]['summary_text']
